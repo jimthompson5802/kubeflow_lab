6fb582da59be5a324bd09f144e8fdb54cd819bc2
Jim Thompson <jimthompson5802@gmail.com>
2021-11-29 07:32:36 -0500

    add setup instructions

---
+++ setup/README.md
@@ -0,0 +1,6 @@
+# instructions for setting up environment
+
+## setup configuration files
+``` 
+bash ./set_me_up.sh
+```
---
+++ setup/set_me_up.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# setup global git config
+git config --global user.email "jimthompson5802@gmail.com"
+git config --global user.name "Jim Thompson"
+
+# setup credential helper for 2 hour time limit
+git config credential.helper 'cache --timeout=7200'
