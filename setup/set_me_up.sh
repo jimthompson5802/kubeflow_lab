8c7114a2f17818db508a847fba855e6b626bf8b8
Jim Thompson <jimthompson5802@gmail.com>
2021-12-05 18:01:49 -0500

    add command alias and bashrc setup

--- setup/set_me_up.sh
+++ setup/set_me_up.sh
@@ -6,3 +6,7 @@ git config --global user.name "Jim Thompson"
 
 # setup credential helper for 2 hour time limit
 git config credential.helper 'cache --timeout=7200'
+
+
+# setup bashrc
+cat dot_bashrc >> ~/.bashrc
